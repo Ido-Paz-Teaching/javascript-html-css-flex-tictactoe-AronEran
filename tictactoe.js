921faa9cd75b73ae43a6df20fffb3d42222abdf9
AronEran <aroneran@example.com>
2021-12-02 19:22:53 +0200

    update

--- tictactoe.js
+++ tictactoe.js
@@ -1,26 +1,72 @@
 //Cells winning (horizontally || Vertically || diagonally) options:
 ///////////////['0','1','2','3','4','5','6','7','8'];
-//Horizontally1['x','x','x',' ',' ',' ',' ',' ',' '];
-//Horizontally2[' ',' ',' ','x','x','x',' ',' ',' '];
-//Horizontally3[' ',' ',' ',' ',' ',' ','x','x','x'];
+var Horizontally1 = ['x','x','x',"","","","","",""];
+var Horizontally2 = ["","","",'x','x','x',"","",""];
+var Horizontally3 = ["","","","","","",'x','x','x'];
 //
-////Vertically1['x',' ',' ','x',' ',' ','x',' ',' '];
-////Vertically2[' ','x',' ',' ','x',' ',' ','x',' '];
-////Vertically3[' ',' ','x',' ',' ','x',' ',' ','x'];
+var Vertically1 = ['x',"","",'x',"","",'x',"",""];
+var Vertically2 = ["",'x',"","",'x',"","",'x',""];
+var Vertically3 = ["","",'x',"","",'x',"","",'x'];
 //
-////Diagonally1['x',' ',' ',' ','x',' ',' ',' ','x'];
-////Diagonally2[' ',' ','x',' ','x',' ','x',' ',' '];
+var Diagonally1 = ['x',"","","",'x',"","","",'x'];
+var Diagonally2 = ["","",'x',"",'x',"",'x',"",""];
 //
+winOptions = [Horizontally1,Horizontally2,Horizontally3,Vertically1,Vertically2,      Vertically3,Diagonally1,Diagonally2];
+
 //recieve a string array parameter and return true
 //if the game is a draw or false if not.
 function hasDraw(selectionArray) {
-   return false;
+    if(areAllButtonsMarked(selectionArray) && count == maxMOVES)
+        return true
+    return false;
 }
+
+function areAllButtonsMarked(selectionArray)
+{
+    for(n in selectionArray)
+        if(selectionArray[n] == "")
+            return false;
+    return true;
+}
+
 //recieve a string array parameter and return true 
 //if someone won the game or false if not.
 function hasWon(selectionArray) {
+    if(sequenceCheck(selectionArray))
+        return true;
+    else
+        {
+            winningCellsFlipp();
+            return false;
+        }
+}
+
+// Change the Horizontally ,Vertically and Diagonally arrays to hold "x" or "0"
+function winningCellsFlipp()
+{   
+    for(let i=0; i<winOptions.length; i++) 
+        for(let j=0; j<winOptions[i].length; j++)
+            if(winOptions[i][j] == temp) 
+                winOptions[i][j] = player; 
+            else if(winOptions[i][j] == player)  
+                winOptions[i][j] = temp;
+}
+
+// Check 'selectionArray' for winning sequence
+function sequenceCheck (selectionArray)
+{   
+    for(let i=0; i<winOptions.length; i++) 
+        for(let j=0,match=0; j<winOptions[i].length; j++)
+            if(winOptions[i][j] == temp)
+                if(selectionArray[j] == temp)
+                {
+                    match++;
+                    if(match == 3)
+                        return true;
+                }
     return false;
 }
+
 //Please , don't modify the following code
 if (typeof module !== "undefined") {
     module.exports = {
--- tictactoeUI.js
+++ tictactoeUI.js
@@ -4,3 +4,93 @@ enter code to handle:
 2. game status update messages
 3. Start button click
 */
+
+
+
+// 'player' valus are "x" or "o"
+var player = "x";
+// Count players moves in the game
+var count =  0;
+// Holds all buttons
+var buttonArray = [];
+// Holds marked ("x" or "o") and positions of buttons
+const followUp = ["","","","","","","","",""];
+// Holds temporary player mark
+var temp;
+// Footer - status messages
+var state;
+// At least 5 moves are needed for one of the players to win
+const minMOVES = 5;
+// Max players moves in the game
+const maxMOVES = 9;
+
+window.addEventListener('load',init);
+
+function init()
+{    
+    const board = document.getElementById('board');
+    buttonArray = board.getElementsByTagName('button');
+    const start = document.querySelector("header button");
+    state = document.getElementById("gameStatusTag");
+    for(let i=0; i<buttonArray.length; i++)
+    {
+        buttonArray[i].innerText = "";
+        buttonArray[i].removeEventListener("click", onButtonClick);
+        buttonArray[i].addEventListener("click", onButtonClick);
+    }
+    start.removeEventListener("click", onStartClick);
+    start.addEventListener("click", onStartClick);
+    state.innerText = "Game on : x - play"
+}
+
+function onButtonClick(eventArg)
+{
+    let btn = eventArg.currentTarget;
+    if(btn.innerText == "")
+    {
+        btn.innerText = player;
+        temp = player;
+        if(player == "x")
+            player = "o";
+        else
+            player = "x"
+        followUp[btn.id[1]] = temp;
+        state.innerText = player + " - play";        
+        count++;
+        if(count >= minMOVES)
+            if(hasWon(followUp))
+            {
+                state.innerHTML = `Game Over:  ${temp} wins<br>
+                To restart - Press the start button `;
+                for(let n in buttonArray)
+                    buttonArray[n].disabled = true
+            }
+            else if(hasDraw(followUp))
+            {
+                state.innerText = "Game Over: It's a Draw";
+                for(let n in buttonArray)
+                    buttonArray[n].disabled = true
+            }
+    }
+    else
+        state.innerHTML = `${player} - play <br>Select only unmarked buttons`;
+} 
+
+function onStartClick(eventArg)
+{
+    count = 0;
+    player = "x";
+    state.innerText = "Game on : x - play"
+    for(let n in buttonArray)
+        buttonArray[n].innerText = "";
+    for(let n in followUp)
+        followUp[n] = "";
+
+    for(let i=0; i<winOptions.length; i++) 
+        for(let j=0; j<winOptions[i].length; j++)
+            if(winOptions[i][j] == "o") 
+                winOptions[i][j] = player;
+    
+    for(let n in buttonArray)
+        buttonArray[n].disabled = false;
+}   
